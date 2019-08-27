e07777760a4c5fa1118d6bde7916c1ff60cdc429
kennylam91 <phamvanlam1991@gmail.com>
2019-08-27 15:26:21 +0700

    create project

---
+++ src/test/java/com/springboot/accessingdatarest/AccessingDataRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.accessingdatarest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccessingDataRestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
